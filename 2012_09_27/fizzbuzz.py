a10d281fbb2450c04a39bae887cd522abec9e49a
Priscila Manhaes <psilva@iff.edu.br>
2012-09-27 23:23:47 -0300

    dojo do dia 27/09/2012 fizzbuzz

---
+++ 2012_09_27/fizzbuzz.py
@@ -0,0 +1,9 @@
+#def fizzbuzz(n):
+#  resultado = ""
+#  if numero % 3 == 0:
+#    resultado += "Fizz"
+#  if numero % 5 == 0:
+#    resultado += "Buzz"
+  
+fizzbuzz = lambda n: (n%15==0 and "FizzBuzz") or (n%3==0 and "Fizz") or (n%5==0 and "Buzz") or ''
+
